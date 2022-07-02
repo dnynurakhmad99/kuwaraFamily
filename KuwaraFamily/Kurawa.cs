98b7f2397068ead5db9a8c55b15d43ff43ae6793
dnynurakhmad99 <80521202+dnynurakhmad99@users.noreply.github.com>
2022-07-02 22:22:43 +0700

    Add files via upload

---
+++ KuwaraFamily/Kurawa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuwaraFamily
+{
+    class Kurawa
+    {
+        public void hit()
+        {
+            Console.WriteLine("Kurawa is hitting");
+        }
+
+        public void kick()
+        {
+            Console.WriteLine("Kurawa is kick");
+        }
+
+        public void defend()
+        {
+            Console.WriteLine("Kurawa is defend");
+        }
+
+        public void dead()
+        {
+            Console.WriteLine("Kurawa is dead");
+        }
+    }
+}
---
+++ KuwaraFamily/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuwaraFamily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Duryodana duryodana = new Duryodana();
+            Dursasana dursasana = new Dursasana();
+            Karna karna = new Karna();
+            Bima bima = new Bima();
+
+            duryodana.hit();
+            dursasana.kick();
+            karna.defend();
+            bima.dead();
+        }
+    }
+}
